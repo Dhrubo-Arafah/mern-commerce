7f23a8d3b38d07796a8d8c048a85452d2a61c8fe
Dhrubo <samiul15-13051@diu.edu.bd>
2021-06-02 05:22:27 +0600

    User Router and Sign Up

--- Backend/app.js
+++ Backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const morgan = require('morgan');
+const userRouter = require('./routers/userRouter')
 
 app.use(express.json());
 app.use(cors());
@@ -10,5 +11,6 @@ if(process.env.NODE_ENV==='development')
 {
   app.use(morgan('dev'));
 }
+app.use('/api/user', userRouter);
 
 module.exports = app;
